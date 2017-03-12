6a8240f52ebade61dac97cfc971543350a7564c5
autoever.meal <autoever.meal@gmail.com>
2017-03-12 12:22:22 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testapp::Application.config.secret_key_base = '3b162acdf1bba9795a4edc4d7a66e915781eeb0a3c1f68b46b39f7fb6be6018dc317bb4d5e20e1e223c335c5dcb9916b70438ba94af4291c6dbe1bf1ecb81150'
